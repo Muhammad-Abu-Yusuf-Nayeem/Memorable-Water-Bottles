f17781128900e4b3968d5b99e9e98b6c7241ab10
assignment-01 <ykhan9463@gmail.com>
2025-08-19 23:14:49 +0600

    use effect dependency and load storage id

--- src/components/Bottles/Bottles.jsx
+++ src/components/Bottles/Bottles.jsx
@@ -1,20 +1,40 @@
 import { useEffect, useState } from "react";
 import Bottle from "../Bottle/Bottle";
 import "./Bottles.css";
+import { getStoredCart } from "../utilities/localStorage";
 
 const Bottles = () => {
   const [bottles, setBottles] = useState([]);
+  const [cart, setCart] = useState([]);
+
   useEffect(() => {
     fetch("bottles.json")
       .then((res) => res.json())
       .then((data) => setBottles(data));
   }, []);
+
+  // load cart from local storage
+  useEffect(() => {
+    const storedCart = getStoredCart();
+    console.log(storedCart);
+  }, []);
+
+  const handleAddToCart = (bottle) => {
+    const newCart = [...cart, bottle];
+    setCart(newCart);
+  };
+
   return (
     <div>
-      <h2>Bottles here: {bottles.length}</h2>
+      <h2>Bottles Available: {bottles.length}</h2>
+      <h4>Cart: {cart.length}</h4>
       <div className="bottle-container">
         {bottles.map((bottles) => (
-          <Bottle bottle={bottles} key={bottles.id}></Bottle>
+          <Bottle
+            bottle={bottles}
+            key={bottles.id}
+            handleAddToCart={handleAddToCart}
+          ></Bottle>
         ))}
       </div>
     </div>
